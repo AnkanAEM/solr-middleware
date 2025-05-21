486af3f0b84237279c85439b5f34383ce07e52c9
Ankan Ghosh <ankan.ghosh@axeno.co>
2025-05-21 15:13:39 +0530

    Enabled CORS for all domains to test the API

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const indexRoutes = require('./routes/addIndex');
 const deleteRoutes = require('./routes/delete');
 const logger = require('./logger');
 require('./cron_jobs/indexingJob'); // Import the cron job to start it
+const cors = require("cors");
 
+var corsOptions = {
+  origin: ""
+}; // Change this on your production server
+app.use(cors()); // DO not use this on your production server - This enables CORS for all domains
 logger.info('App started');  
 const app = express();
 const port = process.env.PORT || 3000;

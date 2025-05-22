a0248370ded0c2673336dbed986cf1316f1cded1
Ankan Ghosh <ankan.ghosh@axeno.co>
2025-05-22 09:13:17 +0530

    Reindex route

--- routes/reindex.js
+++ routes/reindex.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const solrService = require('../services/solrService');
 const logger = require('../logger');
 const { updateSolrIndex } = require('../services/updateSolrIndexService');
 
